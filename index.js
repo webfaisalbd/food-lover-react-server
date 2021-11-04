95064294184f8e3ac01e6a971de85cdbd6524ebf
unknown <web.faisal.bd@gmail.com>
2021-11-05 00:57:43 +0600

    approved and pendion features added

--- index.js
+++ index.js
@@ -76,6 +76,20 @@ async function run() {
       res.send(getUser);
   });
 
+
+  // approved the status
+  app.put("/users/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    const filter = { _id: ObjectId(id) };
+    const updateDoc = {
+        $set: data,
+    };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.json(result);
+});
+
     // DELETE API
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id;
